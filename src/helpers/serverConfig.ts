b66ac1dda6de8b2d2caff7a0444a3dda3b08fa4c
CCC <chad.crume@gmail.com>
2026-01-21 11:08:21 -0500

    feat: serverConfig will try to use process.env.NEXT_PUBLIC_DEV_URL before defaulting to localhost:3000

--- src/helpers/serverConfig.ts
+++ src/helpers/serverConfig.ts
@@ -5,11 +5,16 @@ export const canUseDOM = !!(
 )
 
 export const getServerSideURL = () => {
-  return process.env.NEXT_PUBLIC_VERCEL_URL
+  const serverSideURL = process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : process.env.VERCEL_PROJECT_PRODUCTION_URL
       ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-      : 'http://localhost:3000'
+      : process.env.NEXT_PUBLIC_DEV_URL
+        ? `http://${process.env.NEXT_PUBLIC_DEV_URL}`
+        : 'http://localhost:3000'
+  // console.log(`process.env.NEXT_PUBLIC_DEV_URL: ${process.env.NEXT_PUBLIC_DEV_URL}`)
+  // console.log(`serverSideURL: ${serverSideURL}`)
+  return serverSideURL
 }
 
 export const getClientSideURL = () => {
@@ -18,7 +23,9 @@ export const getClientSideURL = () => {
     const domain = window.location.hostname
     const port = window.location.port
     // `${window.location.protocol}//${window.location.host}
-    return `${protocol}//${domain}${port ? `:${port}` : ''}`
+    const clientSideURL = `${protocol}//${domain}${port ? `:${port}` : ''}`
+    // console.log(`clientSideURL: ${clientSideURL}`)
+    return clientSideURL
   }
 
   return getServerSideURL()
